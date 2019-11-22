025459cf04d4be99c8a8b94d6d7357faca8c574a
Ivan Porty <ivan.portyankin@gmail.com>
2019-11-21 23:00:34 -0500

    многоступенчатая сборка go

---
+++ dockerfile/go-hello-multistage/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Привет из контейнера с сервисом Go")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
